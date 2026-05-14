02d834eafc426369fdbf705ce73923cd76181d08
kz21616 <kz21616@gmail.com>
2026-05-14 15:28:16 +0000

    Sample Code Test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 FEATURE_X = "Wine"
 FEATURE_Y = "Quality"
 
- class Chart(wx.Frame):
+class Chart(wx.Frame):
     def __init__(self, parent=None, title="Wine Quality"):
         super().__init__(parent, title=title,size=(820,520))
         self.SetMinSize((760,480))
@@ -28,5 +28,38 @@ FEATURE_Y = "Quality"
         self.Show()
 
 
+    def on_open(self, _evt):
+        with wx.FileDialog(
+            self,
+            "Open Wine Quality",
+            wildcard="CSV Files(*.csv|*.csv",
+            style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST,
+        ) as dlg:
+            if dlg.ShowModal() != wx.ID_ok:
+                return
+            path = dlg.GetPath()
 
-        
+        try:
+            df = pd.read_csv(path)
+            want = ["Wine", "Wine Quality"]
+
+            df.columns = [str(c).strip().lower().replace(" ", "_") for c in df.columns]
+            if df.shape[1] ==5 and not set(want).issubset(df.columns):
+                df.columns = want
+
+            for c in ["Wine", "Wine Quality"]:
+                if c not in df.columns:
+                    raise ValueError("CSV must contain 2 features.")
+                
+            # plt.figure(figsize=(7.5,5.0))
+            # for sp, sub in df.groupby("")
+
+
+        except Exception as e:
+            wx.MessageBox(f"Could not load/plot file: \n{e}", "Error", wx.OK | wx.ICON_ERROR)
+
+
+if __name__ == "__main__":
+    app = wx.APP(False)
+    Chart()
+    app.MainLoop()
